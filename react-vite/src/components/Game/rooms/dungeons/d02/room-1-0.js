436af68c45733dc41612c22120e896f3aa03bb98
XSpiritWizardX <117311054+XSpiritWizardX@users.noreply.github.com>
2026-08-19 22:28:12 +0000

    Stage Flux Sovereign forge arena

--- react-vite/src/components/Game/rooms/dungeons/d02/room-1-0.js
+++ react-vite/src/components/Game/rooms/dungeons/d02/room-1-0.js
@@ -1,4 +1,12 @@
 export default {
-  id: "d02:1,0", variant: 0, wallRects: [[2, 2, 4, 1], [10, 2, 4, 1], [3, 7, 3, 1], [10, 7, 3, 1]],
-  assets: [{ type: "obelisk", x: 3, y: 5, color: "#7657c7" }, { type: "obelisk", x: 12, y: 5, color: "#7657c7" }],
+  id: "d02:1,0", name: "Sovereign Forge", variant: 0,
+  wallRects: [[2, 2, 4, 1], [10, 2, 4, 1], [3, 7, 3, 1], [10, 7, 3, 1]],
+  assets: [
+    { type: "dungeonStatue", x: 3, y: 5 },
+    { type: "dungeonStatue", x: 12, y: 5 },
+    { type: "caveTorch", x: 5, y: 2, solid: false },
+    { type: "caveTorch", x: 10, y: 2, solid: false },
+    { type: "dungeonPillar", x: 7, y: 3 },
+    { type: "dungeonPillar", x: 8, y: 3 },
+  ],
 };
