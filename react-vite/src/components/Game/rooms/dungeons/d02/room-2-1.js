d5fdc55308f8cbc51ee4257ab6149a9d24520517
XSpiritWizardX <117311054+XSpiritWizardX@users.noreply.github.com>
2026-08-19 22:27:37 +0000

    Author eastern furnace regulator and cache room

--- react-vite/src/components/Game/rooms/dungeons/d02/room-2-1.js
+++ react-vite/src/components/Game/rooms/dungeons/d02/room-2-1.js
@@ -1,4 +1,11 @@
 export default {
-  id: "d02:2,1", variant: 0, wallRects: [[2, 2, 3, 2], [11, 2, 3, 5], [3, 7, 4, 1]],
-  assets: [{ type: "terminal", x: 6, y: 2 }, { type: "satelliteDish", x: 12, y: 7, color: "#b971da" }],
+  id: "d02:2,1", name: "Exhaust Works", variant: 0,
+  wallRects: [[2, 2, 3, 2], [11, 2, 3, 5], [3, 7, 4, 1]],
+  assets: [
+    { type: "dungeonTerminal", x: 3, y: 5 },
+    { type: "dungeonBarrier", x: 5, y: 5, solid: false },
+    { type: "dungeonPillar", x: 8, y: 2 },
+    { type: "caveBones", x: 11, y: 7, solid: false },
+    { type: "dungeonCrate", x: 12, y: 7, solid: false },
+  ],
 };
