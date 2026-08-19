e742c5314f2dc0bd820fb87dc22da3c8f3e99bbf
XSpiritWizardX <117311054+XSpiritWizardX@users.noreply.github.com>
2026-08-19 22:12:36 +0000

    Strengthen Heartroot Sanctum encounter staging

--- react-vite/src/components/Game/rooms/dungeons/d01/room-1-0.js
+++ react-vite/src/components/Game/rooms/dungeons/d01/room-1-0.js
@@ -2,8 +2,11 @@ export default {
   id: "d01:1,0", name: "Heartroot Sanctum", variant: 0,
   wallRects: [[2, 2, 3, 2], [11, 2, 3, 2], [2, 7, 4, 1], [10, 7, 4, 1]],
   assets: [
-    { type: "dungeonStatue", x: 3, y: 5 }, { type: "dungeonStatue", x: 12, y: 5 },
+    { type: "dungeonStatue", x: 3, y: 5 },
+    { type: "dungeonStatue", x: 12, y: 5 },
     { type: "caveTorch", x: 5, y: 2, solid: false },
     { type: "caveTorch", x: 10, y: 2, solid: false },
+    { type: "crystalLarge", x: 7, y: 3, solid: false },
+    { type: "crystalLarge", x: 8, y: 3, solid: false },
   ],
 };
