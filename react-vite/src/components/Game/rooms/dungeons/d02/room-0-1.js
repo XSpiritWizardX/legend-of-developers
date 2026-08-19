5064f88845349ded22b30e11cbe835f814f21048
XSpiritWizardX <117311054+XSpiritWizardX@users.noreply.github.com>
2026-08-19 22:27:28 +0000

    Author western furnace regulator room

--- react-vite/src/components/Game/rooms/dungeons/d02/room-0-1.js
+++ react-vite/src/components/Game/rooms/dungeons/d02/room-0-1.js
@@ -1,4 +1,11 @@
 export default {
-  id: "d02:0,1", variant: 0, wallRects: [[3, 2, 3, 2], [10, 2, 3, 2], [2, 7, 3, 1], [11, 6, 3, 2]],
-  assets: [{ type: "debugStatue", x: 3, y: 5, color: "#7657c7" }, { type: "codeCrate", x: 12, y: 5 }],
+  id: "d02:0,1", name: "Bellows Gallery", variant: 0,
+  wallRects: [[3, 2, 3, 2], [10, 2, 3, 2], [2, 7, 3, 1], [11, 6, 3, 2]],
+  assets: [
+    { type: "dungeonStatue", x: 3, y: 5 },
+    { type: "dungeonTerminal", x: 12, y: 5 },
+    { type: "dungeonBarrier", x: 10, y: 5, solid: false },
+    { type: "caveTorch", x: 8, y: 2, solid: false },
+    { type: "dungeonPillar", x: 5, y: 7 },
+  ],
 };
